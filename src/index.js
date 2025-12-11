947ce3d359aed3c99637f994ce5fd49cde96babc
patrik.zapletal <patrik.zapletal@shiftmed.com>
2025-12-11 12:12:04 +0100

    final solution

--- src/index.js
+++ src/index.js
@@ -1,45 +1,47 @@
-const { readFile: fsReadFile } = require('node:fs/promises');
+const fs = require('fs');
+const readline = require('readline');
 
 const STARTING_VALUE = 50;
 const INPUT_PATH = 'static/input.txt';
 
-const processData = (data) => {
-    const lines = data.split(/\r?\n|\r|\n/g);
+const startup = async(path) => {
     let actualValue = STARTING_VALUE;
-
-    for (const line in lines) {
-        console.log(line);
-        const direction = line.charAt(0);
-        const amount = line.substring(1);
-
-        //console.log(direction, amount);
-
-        if (direction === 'R') {
-            actualValue = (actualValue + amount) % 100;
-        }
-
-        if (direction === 'L') {
-            if (amount > actualValue) {
-                actualValue = 100 - (amount - actualValue);
-            }
-            else {
-                actualValue = actualValue - amount;
-            }
-        }
-    }
-
-    console.log('The final position is', actualValue);
-}
-
-const startup = async(path, processFun) => {
+    let zeroCounter = 0;
     try {
-        const data = await fsReadFile(path, { encoding: 'utf8' });
-        processFun(data);
+        const fileStream = fs.createReadStream(path);
+
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        for await (const line of rl) {
+            const direction = line.charAt(0);
+            const amount = Number(line.substring(1));
+
+            if (direction === 'R') {
+                actualValue = (actualValue + amount) % 100;
+            }
+
+            if (direction === 'L') {
+                if (amount > actualValue) {
+                    actualValue = 100 - (actualValue - amount);
+                }
+                else {
+                    actualValue = actualValue - amount;
+                }
+
+            }
+            if (actualValue === 0) zeroCounter++;
+        }
+
+        console.log('The final position is:', actualValue);
+        console.log('The zero count is:', zeroCounter);
     }
     catch (err) {
         console.log('There was an error reading an input file.', err);
     }
 }
 
-startup(INPUT_PATH, processData);
+startup(INPUT_PATH);
 
